43a449fe57b996911488c48b1d87dee064da1155
Fred Mitchell <FMitchell75@gmail.com>
2016-03-21 23:24:22 -0400

    Refactoring

--- processor.py
+++ processor.py
@@ -2,30 +2,29 @@
 
 import timeit
 
-experiments = [ 1000, 10000, 100000, 1000000, 10000000 ]
 
+def wrapper(func, *args, **kwargs):
+    def wrapped():
+        return func(*args, **kwargs)
 
-def wrapper (func, *args, **kwargs):
-    def wrapped ():
-        return func (*args, **kwargs)
     return wrapped
 
 
-def doitn (n):
+def doitn(n):
     t = 0
-    for i in range (n+1):
+    for i in range(n + 1):
         t += i
     return t
 
-funcs = list ()
-for n in experiments:
-   funcs.append ((n, (wrapper(doitn, n))))
 
-
-def doit ():
-    for (n,func) in funcs:
-       print ('{:>8} {}'.format (n, min (timeit.repeat (func, repeat=2, number=3))))
+def doit():
+    experiments = [1000, 10000, 100000, 1000000, 10000000]
+    funcs = list()
+    for n in experiments:
+        funcs.append((n, (wrapper(doitn, n))))
+    for (n, func) in funcs:
+        print ('{:>8} {}'.format(n, min(timeit.repeat(func, repeat=2, number=3))))
 
 
 if __name__ == '__main__':
-   doit (); 
+    doit();
